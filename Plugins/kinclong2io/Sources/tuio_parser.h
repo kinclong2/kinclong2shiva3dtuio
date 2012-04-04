39deadde276b2b0edd1cc7cc1689164ae3b36a95
Danang Suharno <kinclong2@gmail.com>
2012-04-04 07:04:09 +0700

    Brute force header replacement update

--- Plugins/kinclong2io/Sources/tuio_parser.h
+++ Plugins/kinclong2io/Sources/tuio_parser.h
@@ -1,7 +1,7 @@
 /**
 The MIT License (MIT)
 
-Copyright (c) 2012 Danang Suharno, Yogyakarta 55143, Indonesia, kinclong2@gmail.com
+Copyright (c) ????????????????????????????????
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
@@ -13,6 +13,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 DEALINGS IN THE SOFTWARE.
+
+
+I don't know who coded these functions and I forgot where did I find it. It is far simpler than tuio.org implementation.
 
 */
 
